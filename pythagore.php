6892cd5992a98dd8b67007ea11eaaca7100ab0b9
fcamara16 <fatoumatacamara1611@gmail.com>
2022-02-11 11:56:06 +0100

    origin main

--- pythagore.php
+++ pythagore.php
@@ -1,2 +1,23 @@
 <?php
+echo hypot(3,4) . "<br>";
+echo hypot (4,6) . "<br>";
+echo hypot (1,3) . "<br>";
+echo sqrt (3*3+4*4)
+
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
